2a7df1464e24b1b97dabca8c8c6231b4a41e6689
hiroki04y <hiroki02y104@icloud.com>
2023-10-03 11:44:53 +0900

    課題2作成

--- ex2/work.py
+++ ex2/work.py
@@ -2,7 +2,27 @@ from math import ceil
 
 def calc_account(m):
     # 実装は入れていません、自分で入れてください
-    pass
+    # 走行距離が0m以下の場合はNoneを返す
+    if m <= 0:
+        return None
+    
+    # 初乗り運賃の設定
+    farst_fare = 610
+    
+    # 走行距離が1700m以下の場合は初乗り運賃を返す
+    if m <= 1700:
+        return farst_fare
+    
+    # 走行距離が1700mを超える場合は追加運賃を計算
+    distance = m - 1700
+    add_fare = (distance // 315) * 80
+    
+    # 初乗り運賃と追加運賃を合計して返す
+    total = farst_fare + add_fare
+    return total
+
+
+    
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
